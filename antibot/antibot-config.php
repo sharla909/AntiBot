1f9cc8ce875b812b4729447ba6598f19058ebf3b
sharla909 <74376153+sharla909@users.noreply.github.com>
2020-11-12 12:54:01 -0600

    Add files via upload

---
+++ antibot/antibot-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author: Nokia 1337
+ * @Date:   2019-09-30 10:55:56
+ * @Last Modified by:   Nokia 1337
+ * @Last Modified time: 2019-10-10 22:07:53
+*/
+$config['apikey'] 			= 'c3b841a78b0053cf058e8386bd2e9701';  	// https://antibot.pw/dashboard/developers
+
+//--- [ PAGE CONFIG ] ---//
+
+$config['bot_block']    	= true; 						// if this domain is visited by bot / vpn / proxy it will be blocked. (Config Setting : https://antibot.pw/dashboard/antibot-blocker)
+$config['redirect_bot'] 	= 'https://google.com';
+$config['username_panel'] 	= 'admin';
+$config['password_panel'] 	= 'CleNgMncret123!!';
